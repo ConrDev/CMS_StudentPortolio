1fe812d211b66672b1e42fe90bca0f6326f7b6e7
SKELIC <c.vantilburg05@gmail.com>
2021-03-29 11:49:19 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Student Portfolio</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="title">
+        <div class="logo">
+            <a href="./index.php">LOGO</a>
+        </div>
+
+        <h1 class="text-center">(Naam)'s Portfolio</h1>
+    </div>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div id="my-nav" class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">About Me</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">C.V.</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Projecten</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <section>
+        <div class="card text-center">
+            <div class="card-body">
+                <h5 class="card-title">Welkom op mijn portfilio!</h5>
+                <p class="card-text">(welkoms bericht)</p>
+                <a href="/pages/about.php" class="btn btn-primary">About Me</a>
+            </div>
+        </div>
+    </section>
+
+    <div class="footer">
+        <p class="copyright">© website name</p>
+        <p class="splitter">|</p>
+        <p class="credits">created with ❤️ by WeDevign</p>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
